6c57b0099a1b8a2f1c2b479fdc7f7a9a8ed9903f
ailona <81778135+ergellert@users.noreply.github.com>
2023-03-22 08:55:15 -0500

    Update Gpt-summerizer.py

--- Gpt-summerizer.py
+++ Gpt-summerizer.py
@@ -7,49 +7,58 @@ from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 
 # OpenAI credentials
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "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"
 
 # Email credentials
-email_address = "YOUR_EMAIL_ADDRESS"
-email_password = "YOUR_EMAIL_PASSWORD"
+email_address = "ailona@ailona.com"
+email_password = "J@ckL0rdistough"
+email_toaddress = "erichgellert@gmail.com"
 
 # RSS feed URL
 feed_url = "https://techcommunity.microsoft.com/plugins/custom/microsoft/o365/custom-blog-rss?tid=-2665027495767423092&board=MicrosoftEndpointManagerBlog&size=25"
 
 # Check for new posts
 feed = feedparser.parse(feed_url)
-latest_post = feed.entries[0]
-with open("latest_post.txt", "r+") as file:
+latest_post = feed.entries[4]
+with open("lastpost.txt", "r+") as file:
     latest_post_saved = file.read()
     if latest_post_saved != latest_post.link:
         # Extract text from new post link using Beautiful Soup
         response = requests.get(latest_post.link)
         soup = BeautifulSoup(response.content, "html.parser")
-        text = soup.find("div", attrs={"id": "bodydisplay"}).get_text()
+        text = soup.find("div", attrs={"class": "lia-message-body"}).get_text()
+        
 
-        # Get summary of text using OpenAI API
-        summary = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=text,
-            max_tokens=60,
-            n=1,
-            stop=None,
-            temperature=0.5
-        ).choices[0].text
+        try:
+            summary = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=f"Please summarize this for a second-grade student:\n{text}\n\nSummary:",
+                temperature=0.7,
+                max_tokens=64,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+                ).choices[0].text
+        except Exception as e:
+            print("Error generating summary:", e)
+            summary = "Error generating summary"
 
-        # Compose email with summary
-        message = MIMEText(summary)
+         # Compose email with summary and link
+        message_body = f"{summary}\n\n{latest_post.link}"
+        message = MIMEText(message_body)
         message["Subject"] = latest_post.title
         message["From"] = email_address
-        message["To"] = email_address
+        message["To"] = "erichgellert@gmail.com, ergeller@microsoft.com, russ.rimmerman@microsoft.com"
+        
 
         # Send email
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        with smtplib.SMTP("10.0.0.67", 587) as server:
             server.starttls()
             server.login(email_address, email_password)
-            server.sendmail(email_address, email_address, message.as_string())
+            server.sendmail(email_address, email_toaddress, message.as_string())
 
         # Update latest post saved
         file.seek(0)
         file.write(latest_post.link)
         file.truncate()
+
